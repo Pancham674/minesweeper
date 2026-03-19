799b071ed6a1a2b6fcceb14d447cd38a1e3c8963
Pancham674 <pamela.p00@hotmail.de>
2026-03-19 22:48:42 +0100

    ~ Renamed almost every html mention to view, custom sizes will be applied when confirmed,

--- minesweeper/wwwroot/js/game.js
+++ minesweeper/wwwroot/js/game.js
@@ -1,9 +1,8 @@
 ﻿let _boardModel;
-let _boardHtml;
-let _boardHtmlTbody;
-//do _boardHtmlTbody.children[Row].cells[Column].children[0] to get the html element of the cellModel 
+let _boardView;
+let _boardViewTableBody;
+//do _boardViewTableBody.children[Row].cells[Column].children[0] to get the view element of the cellModel 
 
-let _currentBoardSize;
 let _fieldAmount;
 
 //reuse lifes in future rounds
@@ -29,9 +28,9 @@ const _noNeighboringBombs = "";
 
 
 function refreshBoardElements() {
-    _boardHtml = document.getElementById('board');
-    _boardModel = JSON.parse(_boardHtml.dataset.board);
-    _boardHtmlTbody = _boardHtml.firstElementChild.firstElementChild;
+    _boardView = document.getElementById('board');
+    _boardModel = JSON.parse(_boardView.dataset.board);
+    _boardViewTableBody = _boardView.firstElementChild.firstElementChild;
 
     _bombsInGF = _boardModel.BombCount;
     _fieldAmount = _boardModel.Rows * _boardModel.Columns;
@@ -45,31 +44,31 @@ function refreshBoardElements() {
     _boardProgress.textContent = `Covered fields: ${_fieldAmount - _bombsInGF}/${_fieldAmount - _bombsInGF}`;
     _lifes.textContent = `Lifes: ${_lifeAmount}`;
 
-    _boardHtml.oncontextmenu = (e) => { e.preventDefault(); }
+    _boardView.oncontextmenu = (e) => { e.preventDefault(); }
 
-    _boardHtml.addEventListener('onCellLeftClick', e => {
+    _boardView.addEventListener('onCellLeftClick', e => {
         const { column, row } = e.detail;
-        onFieldClicked(_boardModel.Cells[column][row], _boardHtmlTbody.children[row].cells[column].children[0]);
+        onFieldClicked(_boardModel.Cells[column][row], _boardViewTableBody.children[row].cells[column].children[0]);
     });
 
-    _boardHtml.addEventListener('onCellRightClick', e => {
+    _boardView.addEventListener('onCellRightClick', e => {
         const { column, row } = e.detail;
-        setFlag(_boardModel.Cells[column][row], _boardHtmlTbody.children[row].cells[column].children[0]);
+        setFlag(_boardModel.Cells[column][row], _boardViewTableBody.children[row].cells[column].children[0]);
     });
 
-    _boardHtml.addEventListener('onCellHover', e => {
+    _boardView.addEventListener('onCellHover', e => {
         const { column, row } = e.detail;
-        onFieldEnter(_boardModel.Cells[column][row], _boardHtmlTbody.children[row].cells[column].children[0]);
+        onFieldEnter(_boardModel.Cells[column][row]);
     });
 
-    _boardHtml.addEventListener('onCellHoverEnded', e => {
+    _boardView.addEventListener('onCellHoverEnded', e => {
         const { column, row } = e.detail;
-        onFieldLeave(_boardModel.Cells[column][row], _boardHtmlTbody.children[row].cells[column].children[0]);
+        onFieldLeave(_boardModel.Cells[column][row]);
     });
 }
 
 //will reveal the clicked field and performs a specific action based on its attributes
-function onFieldClicked(cellModel, cellHtml) {
+function onFieldClicked(cellModel, cellView) {
     _roundStarted = true;
 
     if (cellModel.IsFlagged) {
@@ -82,8 +81,8 @@ function onFieldClicked(cellModel, cellHtml) {
         //treat bomb as flag for chording
         cellModel.IsFlagged = true;
         cellModel.IsExploded = true;
-        cellHtml.onclick = () => { };
-        cellHtml.oncontextmenu = (e) => { e.preventDefault() };
+        cellView.onclick = () => { };
+        cellView.oncontextmenu = (e) => { e.preventDefault() };
 
         if (_lifeAmount <= 0) {
             revealBoard(false);
@@ -93,17 +92,17 @@ function onFieldClicked(cellModel, cellHtml) {
     }
     // player wants to use chord
     else if (cellModel.IsRevealed) {
-        fieldChord(cellModel, cellHtml);
+        fieldChord(cellModel, cellView);
         return;
     }
 
     cellModel.IsRevealed = true;
-    cellHtml.textContent = cellModel.IsBomb ? "💣" : cellModel.NeighboringBombs;
-    cellHtml.className = cellModel.NeighboringBombs == 0 ? "empty field" : "field";
+    cellView.textContent = cellModel.IsBomb ? "💣" : cellModel.NeighboringBombs;
+    cellView.className = cellModel.NeighboringBombs == 0 ? "empty field" : "field";
     changeBoardProgress(cellModel.IsBomb);
 
     if (cellModel.NeighboringBombs == 0) {
-        uncoverNeighboringFields(cellModel, cellHtml);
+        uncoverNeighboringFields(cellModel, cellView);
     }
 
     if (isGameFinished()) {
@@ -114,7 +113,7 @@ function onFieldClicked(cellModel, cellHtml) {
 }
 
 //uncover neighbors if the flags on the neighbors is equal/greater than the neigboring bombs
-function fieldChord(cellModel, cellHtml) {
+function fieldChord(cellModel, cellView) {
     let flaggedNeighbors = 0;
     let neighbors = [];
 
@@ -130,33 +129,33 @@ function fieldChord(cellModel, cellHtml) {
             }
 
             const neighborModel = _boardModel.Cells[cCol][cRow];
-            const neighborHtml = _boardHtmlTbody.children[cRow].cells[cCol].children[0];
+            const neighborView = _boardViewTableBody.children[cRow].cells[cCol].children[0];
 
             //ignore self
             if (neighborModel === cellModel) {
                 continue;
             }
             flaggedNeighbors = neighborModel.IsFlagged ? flaggedNeighbors + 1 : flaggedNeighbors;
-            neighbors.push({ model: neighborModel, html: neighborHtml });
+            neighbors.push({ model: neighborModel, view: neighborView });
         }
     }
 
-    //check if theres a correct amount of flags placed around it
+    //check if theres an equal or greater amount of flags placed around it
     if (flaggedNeighbors >= cellModel.NeighboringBombs) {
-        neighbors.forEach(index  => {
-            //ignore self, uncovered and flagged fields
-            if (index.model === cellModel || index.model.IsRevealed || index.model.IsFlagged) {
+        neighbors.forEach(index => {
+            //ignore uncovered and flagged fields
+            if (index.model.IsRevealed || index.model.IsFlagged) {
                 return;
             }
 
             //uncover neigbors
-            onFieldClicked(index.model, index.html);
+            onFieldClicked(index.model, index.view);
         })
     }
 }
 
 //highlight the neigbors of an uncovered field by adding a classname
-function onFieldEnter(cellModel, cellHtml) {
+function onFieldEnter(cellModel) {
     //only neighbors of uncovered fields should be marked
     if (!cellModel.IsRevealed) {
         return;
@@ -174,15 +173,15 @@ function onFieldEnter(cellModel, cellHtml) {
             }
 
             const neighborModel = _boardModel.Cells[cCol][cRow];
-            const neighborHtml = _boardHtmlTbody.children[cRow].cells[cCol].children[0];
+            const neighborView = _boardViewTableBody.children[cRow].cells[cCol].children[0];
 
             //ignore self, revealed cells and flagged cells since they wont be marked
             if (neighborModel === cellModel || neighborModel.IsRevealed || neighborModel.IsFlagged) {
                 continue;
             }
 
-            if (!neighborHtml.className.includes(" hovering")) {
-                neighborHtml.className += " hovering";
+            if (!neighborView.className.includes(" hovering")) {
+                neighborView.className += " hovering";
             }
         }
     }
@@ -206,15 +205,15 @@ function onFieldLeave(cellModel) {
             }
 
             const neighborModel = _boardModel.Cells[cCol][cRow];
-            const neighborHtml = _boardHtmlTbody.children[cRow].cells[cCol].children[0];
+            const neighborView = _boardViewTableBody.children[cRow].cells[cCol].children[0];
 
             //ignore self, revealed cells and flagged cells since they wont be marked
             if (neighborModel === cellModel || neighborModel.IsRevealed || neighborModel.IsFlagged) {
                 continue;
             }
 
-            if (neighborHtml.className.includes(" hovering")) {
-                neighborHtml.className = neighborHtml.className.substring(0, neighborHtml.className.length - " hovering".length);
+            if (neighborView.className.includes(" hovering")) {
+                neighborView.className = neighborView.className.substring(0, neighborView.className.length - " hovering".length);
             }
         }
     }
@@ -228,9 +227,9 @@ function changeBoardProgress(fieldWasBomb) {
 }
 
 //uncovers neighbors if the current field has no neigboring bombs
-function uncoverNeighboringFields(cellModel, cellHtml) {
-    cellHtml.onclick = () => { };
-    cellHtml.oncontextmenu = (cursor) => {
+function uncoverNeighboringFields(cellModel, cellView) {
+    cellView.onclick = () => { };
+    cellView.oncontextmenu = (cursor) => {
         cursor.preventDefault();
     };
 
@@ -246,7 +245,7 @@ function uncoverNeighboringFields(cellModel, cellHtml) {
             }
 
             const neighborModel = _boardModel.Cells[cCol][cRow];
-            const neighborHtml = _boardHtmlTbody.children[cRow].cells[cCol].children[0];
+            const neighborView = _boardViewTableBody.children[cRow].cells[cCol].children[0];
 
             //ignore self and uncovered fields
             if (neighborModel === cellModel || neighborModel.IsRevealed) {
@@ -254,8 +253,8 @@ function uncoverNeighboringFields(cellModel, cellHtml) {
             }
 
             neighborModel.IsRevealed = true;
-            neighborHtml.className = "field";
-            neighborHtml.textContent = neighborModel.NeighboringBombs;
+            neighborView.className = "field";
+            neighborView.textContent = neighborModel.NeighboringBombs;
             changeBoardProgress();
 
             //give player their flag back
@@ -267,8 +266,8 @@ function uncoverNeighboringFields(cellModel, cellHtml) {
 
             //continue uncovering fields that have no bombs as neigbors!!!
             if (neighborModel.NeighboringBombs == _noNeighboringBombs) {
-                neighborHtml.className = "empty field";
-                uncoverNeighboringFields(neighborModel, neighborHtml);
+                neighborView.className = "empty field";
+                uncoverNeighboringFields(neighborModel, neighborView);
             }
         }
     }
@@ -306,12 +305,12 @@ function isGameFinished() {
 //reveals all bombs and disables events of all fields
 //will only be called when the game is finished by winning or losing
 function revealBoard(wonGame) {
-    _boardHtml.className = wonGame ? _boardHtml.className + "won" : _boardHtml.className;
+    _boardView.className = wonGame ? _boardView.className + "won" : _boardView.className;
     _lossStreakCount = wonGame ? 0 : _lossStreakCount + 1;
 
     _boardModel.Cells.forEach((row) => {
         row.forEach((cellModel) => {
-            const cellView = _boardHtmlTbody.children[cellModel.Row].cells[cellModel.Column].children[0];
+            const cellView = _boardViewTableBody.children[cellModel.Row].cells[cellModel.Column].children[0];
 
             cellView.removeEventListener('click', dispatchLeftClick);
             cellView.removeEventListener('contextmenu', dispatchRightClick);
@@ -338,7 +337,7 @@ function revealBoard(wonGame) {
     });
 }
 
-function setFlag(cellModel, cellHtml) {
+function setFlag(cellModel, cellView) {
     if (cellModel.IsRevealed) {
         return;
     }
@@ -347,13 +346,13 @@ function setFlag(cellModel, cellHtml) {
     if (cellModel.IsFlagged) {
         _setFlags--;
         cellModel.IsFlagged = false;
-        cellHtml.textContent = "";
+        cellView.textContent = "";
     }
     //only add a flag if the amount of set flags is lesser than the amount of bombs
     else if (_setFlags < _bombsInGF) {
         _setFlags++;
         cellModel.IsFlagged = true;
-        cellHtml.textContent = "🚩";
+        cellView.textContent = "🚩";
     }
 
     _remainingFlags.textContent = `Remaining flags: ${_bombsInGF - _setFlags}/${_bombsInGF}`;
@@ -370,7 +369,7 @@ function confirmCustomSize() {
         return;
     }
 
-    resetGame(`${customRow}x${customColumn}`);
+    resetGame(this, customColumn, customRow);
 }
 
 //change the life amount and reset the game
@@ -392,11 +391,15 @@ function confirmLifeAmount() {
     _lifes.textContent = `Lifes: ${_lifeAmount}`;
 
     changeTitlesOnLoss();
-    setUpField(_currentBoardSize);
+    resetGame(this, _boardModel.Columns, _boardModel.Rows);
 }
 
 function changeTitlesOnLoss() {
+    if (_lossStreakCount == 0) {
+        return;
+    }
+
     changeTitles("Good luck!", [_lossStreakCount + 1 + (_lossStreakCount + 1 == 1 ? "st " :
-        _lossStreakCount + 1 == 2 ? "nd " :
-            _lossStreakCount + 1 == 3 ? "rd " : "th ") + "try's a charm"]);
+                                                        _lossStreakCount + 1 == 2 ? "nd " :
+                                                        _lossStreakCount + 1 == 3 ? "rd " : "th ") + "try's a charm"]);
 }
